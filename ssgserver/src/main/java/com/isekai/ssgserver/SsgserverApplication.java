0fe9c1569a631d587e7567b5f335ce3c21b23e07
hayoonpark <the.day0619@gmail.com>
2024-03-06 10:52:45 +0900

    Init: Backend 프로젝트 생성

---
+++ ssgserver/src/main/java/com/isekai/ssgserver/SsgserverApplication.java
@@ -0,0 +1,13 @@
+package com.isekai.ssgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgserverApplication.class, args);
+	}
+
+}
---
+++ ssgserver/src/test/java/com/isekai/ssgserver/SsgserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isekai.ssgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
