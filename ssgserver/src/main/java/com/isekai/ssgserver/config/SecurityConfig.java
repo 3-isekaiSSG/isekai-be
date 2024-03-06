fe9172acd3c89356452daa4287fe401506cff2f6
hayoonpark <the.day0619@gmail.com>
2024-03-06 11:57:59 +0900

    Feat: Security 설정

---
+++ ssgserver/src/main/java/com/isekai/ssgserver/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.isekai.ssgserver.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    // Spring Security 검사 비활성화
+    @Bean
+    public WebSecurityCustomizer configure() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**");
+    }
+
+    // Http Security
+    @Bean
+    public SecurityFilterChain filterChai(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll()  // 모든 요청에 모든 사용자 접근 허용(개발 편의를 위해 설정)
+                .and().build();
+
+    }
+}
