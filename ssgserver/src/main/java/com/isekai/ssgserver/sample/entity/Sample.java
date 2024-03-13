57a816a9af9dcfea51b67f675b2cad8e6dd47595
mirim <mirimy97@gmail.com>
2024-03-13 21:37:27 +0900

    Infra: 배포테스트

--- ssgserver/src/main/java/com/isekai/ssgserver/sample/entity/Sample.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/sample/entity/Sample.java
@@ -1,18 +1,18 @@
 package com.isekai.ssgserver.sample.entity;
 
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-//@Entity
-//@Getter
-//@NoArgsConstructor
+@Entity
+@Getter
+@NoArgsConstructor
 public class Sample {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.AUTO)
-//    private Long samplePk;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long samplePk;
 }
--- ssgserver/src/main/java/com/isekai/ssgserver/sample/repository/SampleRepository.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/sample/repository/SampleRepository.java
@@ -1,11 +1,10 @@
 package com.isekai.ssgserver.sample.repository;
 
 import com.isekai.ssgserver.sample.entity.Sample;
-//import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-//public interface SampleRepository extends JpaRepository<Sample, Long> {
-public interface SampleRepository {
+public interface SampleRepository extends JpaRepository<Sample, Long> {
 
 }
