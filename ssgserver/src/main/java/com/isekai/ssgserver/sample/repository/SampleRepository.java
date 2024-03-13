55e71a628e3f0f3f09c8ebf668bb480b2008b2ae
mirim <mirimy97@gmail.com>
2024-03-13 21:24:17 +0900

    Infra: 배포테스트

--- ssgserver/src/main/java/com/isekai/ssgserver/sample/repository/SampleRepository.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/sample/repository/SampleRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SampleRepository extends JpaRepository<Sample, Long> {
+//public interface SampleRepository extends JpaRepository<Sample, Long> {
+public interface SampleRepository {
 
 }
