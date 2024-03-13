0e38a632b7de118738fc46859b5450eb3b2a8da5
mirim <mirimy97@gmail.com>
2024-03-13 12:03:23 +0900

    Feat: Redis 환경설정

---
+++ ssgserver/src/main/java/com/isekai/ssgserver/redis/RedisTestController.java
@@ -0,0 +1,33 @@
+package com.isekai.ssgserver.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedisTestController {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @PostMapping("/redisTest")
+    public ResponseEntity<?> addRedisKey() {
+        ValueOperations<String, String> vop = redisTemplate.opsForValue();
+        vop.set("mirim", "jang");
+        vop.set("jang", "mirim");
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/redisTest/{key}")
+    public ResponseEntity<?> getReisKey(@PathVariable String key) {
+        ValueOperations<String, String> vop = redisTemplate.opsForValue();
+        String value = vop.get(key);
+        return ResponseEntity.ok(value);
+    }
+
+}
