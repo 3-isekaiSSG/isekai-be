2e509063acab2b496b676c00c1ac3ec3d645128a
hayoonpark <the.day0619@gmail.com>
2024-03-14 16:15:56 +0900

    Chore: Swagger 설정 수정

--- ssgserver/src/main/java/com/isekai/ssgserver/config/SwaggerConfig.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/config/SwaggerConfig.java
@@ -1,20 +1,26 @@
 package com.isekai.ssgserver.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("isekai Project API Document")
-                        .description("isekai sgg.com 클론코딩의 API 명세서입니다.")
-                        .version("1.0")
-                );
-    }
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI().addServersItem(new Server().url("/"))
+			.components(new Components().addSecuritySchemes("basicScheme",
+				new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+			.info(new Info()
+				.title("isekai Project API Document")
+				.description("isekai sgg.com 클론코딩의 API 명세서입니다.")
+				.version("1.0")
+			);
+	}
 }
