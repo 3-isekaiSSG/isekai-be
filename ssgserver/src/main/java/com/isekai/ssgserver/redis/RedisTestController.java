aff725dd9222ae72a451c316df9281b59e0b7022
mirim <mirimy97@gmail.com>
2024-03-18 10:34:09 +0900

    Docs: 스웨거 API 설명 추가

--- ssgserver/src/main/java/com/isekai/ssgserver/redis/RedisTestController.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/redis/RedisTestController.java
@@ -1,5 +1,6 @@
 package com.isekai.ssgserver.redis;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -19,14 +20,16 @@ public class RedisTestController {
     private RedisTemplate<String, String> redisTemplate;
 
     @PostMapping("/redisTest")
+    @Operation(summary = "Redis 데이터 저장 테스트", description = "저장이 제대로 이루어지는지 확인합니다. (저장되는 값은 'banana-yellow', 'apple-red'로 고정)")
     public ResponseEntity<?> addRedisKey() {
         ValueOperations<String, String> vop = redisTemplate.opsForValue();
-        vop.set("mirim", "jang");
-        vop.set("jang", "mirim");
+        vop.set("banana", "yellow");
+        vop.set("apple", "red");
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/redisTest/{key}")
+    @Operation(summary = "Redis key-value 조회", description = "입력한 key값으로 저장된 value를 조회합니다.")
     public ResponseEntity<?> getReisKey(@PathVariable String key) {
         ValueOperations<String, String> vop = redisTemplate.opsForValue();
         String value = vop.get(key);
