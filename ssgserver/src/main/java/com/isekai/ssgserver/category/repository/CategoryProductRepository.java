af90ed99f46945c40f95238ec1f93c55bd6cbef9
hayoonPark <the.day0619@gmail.com>
2024-03-23 22:31:47 +0900

    Feat: 중분류 상품조회 repository

---
+++ ssgserver/src/main/java/com/isekai/ssgserver/category/repository/CategoryProductRepository.java
@@ -0,0 +1,19 @@
+package com.isekai.ssgserver.category.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.isekai.ssgserver.category.entity.CategoryProduct;
+import com.isekai.ssgserver.product.entity.Product;
+
+@Repository
+public interface CategoryProductRepository extends JpaRepository<CategoryProduct, Long> {
+
+	// 중분류 상품 조회
+	@Query("SELECT cp.product FROM CategoryProduct cp WHERE (cp.categoryM.categoryMId = :categoryMId)")
+	List<Product> findByCategoryMId(@Param("categoryMId") Long categoryMId);
+}
