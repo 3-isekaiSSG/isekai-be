9d8e2275ea5bfbd7fbb0efe6548ef795fb9f0e1d
hayoonpark <the.day0619@gmail.com>
2024-03-27 09:58:37 +0900

    Chore: querydsl 설정 부분 수정

--- ssgserver/src/main/java/com/isekai/ssgserver/SsgserverApplication.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/SsgserverApplication.java
@@ -2,10 +2,9 @@ package com.isekai.ssgserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.isekai"})
+// @ComponentScan(basePackages = {"com.isekai"})
 public class SsgserverApplication {
 
 	public static void main(String[] args) {
