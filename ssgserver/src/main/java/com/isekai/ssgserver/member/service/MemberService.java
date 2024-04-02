54bd412173616891f84bf4d32ee36a88380c274d
mirim <mirimy97@gmail.com>
2024-04-02 19:03:44 +0900

    Feat: 로그인 기능 Exception 수정 (401->403)

--- ssgserver/src/main/java/com/isekai/ssgserver/member/service/MemberService.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/member/service/MemberService.java
@@ -1,5 +1,11 @@
 package com.isekai.ssgserver.member.service;
 
+import java.util.Optional;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.isekai.ssgserver.exception.common.CustomException;
 import com.isekai.ssgserver.exception.constants.ErrorCode;
 import com.isekai.ssgserver.member.dto.MemberJoinDto;
@@ -8,45 +14,40 @@ import com.isekai.ssgserver.member.entity.Member;
 import com.isekai.ssgserver.member.repository.MemberRepository;
 import com.isekai.ssgserver.util.jwt.JwtProvider;
 import com.isekai.ssgserver.util.jwt.JwtToken;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class MemberService {
-    private final MemberRepository memberRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
-    private final JwtProvider jwtProvider;
+	private final MemberRepository memberRepository;
+	private final BCryptPasswordEncoder passwordEncoder;
+	private final JwtProvider jwtProvider;
 
-    public void isDuplicate(String accountId) {
-        Optional<Member> existingMember = memberRepository.findByAccountId(accountId);
-        if (existingMember.isPresent()) {
-            throw new CustomException(ErrorCode.ALREADY_EXIST_USER);
-        }
-    }
+	public void isDuplicate(String accountId) {
+		Optional<Member> existingMember = memberRepository.findByAccountId(accountId);
+		if (existingMember.isPresent()) {
+			throw new CustomException(ErrorCode.ALREADY_EXIST_USER);
+		}
+	}
 
-    public void join(MemberJoinDto joinDto) {
-        // 가입 정보 저장 + 비밀번호는 암호화해서 저장
-        String encodedPassword = passwordEncoder.encode(joinDto.getPassword());
+	public void join(MemberJoinDto joinDto) {
+		// 가입 정보 저장 + 비밀번호는 암호화해서 저장
+		String encodedPassword = passwordEncoder.encode(joinDto.getPassword());
 
-        Member member = joinDto.toEntity(encodedPassword);
-        memberRepository.save(member);
-    }
+		Member member = joinDto.toEntity(encodedPassword);
+		memberRepository.save(member);
+	}
 
+	public JwtToken login(MemberLoginDto loginDto) {
+		Member member = memberRepository.findByAccountId(loginDto.getAccountId())
+			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
 
-    public JwtToken login(MemberLoginDto loginDto) {
-        Member member = memberRepository.findByAccountId(loginDto.getAccountId())
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+		if (!passwordEncoder.matches(loginDto.getPassword(), member.getPassword())) {
+			throw new CustomException(ErrorCode.PASSWORD_ERROR);
+		}
 
-        if (!passwordEncoder.matches(loginDto.getPassword(), member.getPassword())) {
-            throw new CustomException(ErrorCode.NO_AUTHORITY);
-        }
-
-        return jwtProvider.createToken(member.getUuid());
-    }
+		return jwtProvider.createToken(member.getUuid());
+	}
 }
