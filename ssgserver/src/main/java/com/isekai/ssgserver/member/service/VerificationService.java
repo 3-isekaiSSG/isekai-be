2392e435ce27665f3c38f9533441a77320fcf989
mirim <mirimy97@gmail.com>
2024-04-02 13:40:10 +0900

    Fix: Member관련 통신 오류 테스트

--- ssgserver/src/main/java/com/isekai/ssgserver/member/service/VerificationService.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/member/service/VerificationService.java
@@ -1,24 +1,25 @@
 package com.isekai.ssgserver.member.service;
 
-import com.isekai.ssgserver.member.entity.Member;
-import com.isekai.ssgserver.member.repository.MemberRepository;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.isekai.ssgserver.exception.common.CustomException;
 import com.isekai.ssgserver.exception.constants.ErrorCode;
 import com.isekai.ssgserver.member.dto.VerificationDto;
+import com.isekai.ssgserver.member.entity.Member;
+import com.isekai.ssgserver.member.repository.MemberRepository;
 import com.isekai.ssgserver.member.repository.VerificationRepository;
 import com.isekai.ssgserver.util.PhoneVerificationUtil;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class VerificationService {
 
 	private final PhoneVerificationUtil phoneVerificationUtil;
@@ -33,17 +34,21 @@ public class VerificationService {
 	public void sendSms(VerificationDto.SmsVerificationRequest smsVerificationRequest) {
 		String to = smsVerificationRequest.getPhone();
 
+		log.info(to);
 		// 휴대폰 번호로 기존 회원 조회
 		Optional<Member> existingUser = memberRepository.findByPhone(to);
 		if (existingUser.isPresent()) {
 			// 이미 존재하는 회원인 경우 예외 처리
 			throw new CustomException(ErrorCode.ALREADY_EXIST_USER);
 		}
-
+		log.info("회원 조회 성공");
 		int randomNumber = (int)(Math.random() * 1000000); // 0부터 999999까지의 6자리 숫자
 		String verificationNumber = String.format("%06d", randomNumber); // 6자리 숫자로 포맷
 		phoneVerificationUtil.sendSms(to, verificationNumber);
+		log.info("sendSmS 성공");
+
 		verificationRepository.createSmsVerification(to, verificationNumber);
+		log.info("createSmsVerification 성공");
 	}
 
 	public void verifySms(VerificationDto.SmsVerificationRequest smsVerificationRequest) {
