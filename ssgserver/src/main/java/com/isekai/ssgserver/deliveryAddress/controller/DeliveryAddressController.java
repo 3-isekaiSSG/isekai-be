163de169453f299b53efed3fb1ef12d1be846875
mirim <mirimy97@gmail.com>
2024-04-05 17:08:01 +0900

    Feat: 배송지 정보 조회 API 구현

--- ssgserver/src/main/java/com/isekai/ssgserver/deliveryAddress/controller/DeliveryAddressController.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/deliveryAddress/controller/DeliveryAddressController.java
@@ -3,11 +3,13 @@ package com.isekai.ssgserver.deliveryAddress.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isekai.ssgserver.deliveryAddress.dto.DeliveryAddressNicknameDto;
 import com.isekai.ssgserver.deliveryAddress.service.DeliveryAddressService;
+import com.isekai.ssgserver.util.jwt.JwtProvider;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -21,13 +23,28 @@ import lombok.extern.slf4j.Slf4j;
 @Tag(name = "DeliveryAddress", description = "배송지 관련 API document")
 public class DeliveryAddressController {
 
+	private final JwtProvider jwtProvider;
 	private final DeliveryAddressService deliveryAddressService;
 
 	@GetMapping("/{deliveryAddressId}/nickname")
-	@Operation(summary = "배송지 별칭 조회", description = "사용자 지정 별칭 조회")
+	@Operation(summary = "배송지 별칭 조회", description = "회원/비회원 - 사용자 지정 별칭 조회")
 	public ResponseEntity<DeliveryAddressNicknameDto> getDeliveryAddressNickname(@PathVariable Long deliveryAddressId) {
 
 		DeliveryAddressNicknameDto nicknameDto = deliveryAddressService.getDeliveryAddressNickname(deliveryAddressId);
 		return ResponseEntity.ok(nicknameDto);
 	}
+
+	@GetMapping("/{deliveryAddressId}")
+	@Operation(summary = "배송지 정보 조회", description = "회원/비회원 - 배송지 정보 조회")
+	public ResponseEntity<DeliveryAddressInfoDto> getDeliveryAddressInfo(
+		@RequestHeader(value = "Authorization", required = false) String token,
+		@PathVariable Long deliveryAddressId) {
+
+		String uuid = jwtProvider.getUuid(token);
+
+		DeliveryAddressInfoDto deliveryAddressInfoDto = deliveryAddressService.getDeliveryAddressInfo(uuid,
+			deliveryAddressId);
+		return ResponseEntity.ok(deliveryAddressInfoDto);
+	}
+	
 }
--- ssgserver/src/main/java/com/isekai/ssgserver/deliveryAddress/service/DeliveryAddressService.java
+++ ssgserver/src/main/java/com/isekai/ssgserver/deliveryAddress/service/DeliveryAddressService.java
@@ -2,10 +2,13 @@ package com.isekai.ssgserver.deliveryAddress.service;
 
 import org.springframework.stereotype.Service;
 
+import com.isekai.ssgserver.deliveryAddress.controller.DeliveryAddressInfoDto;
 import com.isekai.ssgserver.deliveryAddress.dto.DeliveryAddressNicknameDto;
+import com.isekai.ssgserver.deliveryAddress.entity.DeliveryAddress;
 import com.isekai.ssgserver.deliveryAddress.repository.DeliveryAddressRepository;
 import com.isekai.ssgserver.exception.common.CustomException;
 import com.isekai.ssgserver.exception.constants.ErrorCode;
+import com.isekai.ssgserver.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 public class DeliveryAddressService {
 
 	private final DeliveryAddressRepository deliveryAddressRepository;
+	private final MemberRepository memberRepository;
 
 	public DeliveryAddressNicknameDto getDeliveryAddressNickname(Long deliveryAddressId) {
 
@@ -26,4 +30,32 @@ public class DeliveryAddressService {
 			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ENTITY));
 	}
 
+	public DeliveryAddressInfoDto getDeliveryAddressInfo(String uuid, Long deliveryAddressId) {
+
+		DeliveryAddress deliveryAddress = deliveryAddressRepository.findById(deliveryAddressId)
+			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ENTITY));
+
+		if (deliveryAddress.getMemberId() != -1L) { // 회원의 배송지인 경우
+			Long savedMemberId = memberRepository.findByUuidAndIsWithdraw(uuid, (byte)0)
+				.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER))
+				.getMemberId();
+
+			if (!savedMemberId.equals(deliveryAddress.getMemberId())) {
+				throw new CustomException(ErrorCode.NO_AUTHORITY);
+			}
+		}
+
+		return DeliveryAddressInfoDto.builder()
+			.nickname(deliveryAddress.getNickname())
+			.name(deliveryAddress.getName())
+			.cellphone(deliveryAddress.getCellphone())
+			.telephone(deliveryAddress.getTelephone())
+			.zipcode(deliveryAddress.getZipcode())
+			.address(deliveryAddress.getAddress())
+			.isDefault(deliveryAddress.isDefault())
+			.isDeleted(deliveryAddress.isDeleted())
+			.orderHistory(deliveryAddress.isOrderHistory())
+			.build();
+
+	}
 }
