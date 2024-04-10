e22c43809630cbfaafd6326e17317795313c330d
hayoonPark <the.day0619@gmail.com>
2024-04-10 23:27:20 +0900

    Feat: 상품 검색 초기 세팅

---
+++ ssgserver/src/main/java/com/isekai/ssgserver/config/ElasticSearchConfig.java
@@ -0,0 +1,35 @@
+package com.isekai.ssgserver.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.client.erhlc.RestClients;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticSearchConfig extends ElasticsearchConfiguration {
+
+	@Value("${spring.data.elasticsearch.url}")
+	String esUrl;
+
+	@Bean
+	public RestHighLevelClient restHighLevelClient() {
+
+		ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+			.connectedTo(esUrl)
+			.build();
+
+		return RestClients.create(clientConfiguration).rest();
+	}
+
+	@Override
+	public ClientConfiguration clientConfiguration() {
+		return ClientConfiguration.builder()
+			.connectedTo(esUrl)
+			.build();
+	}
+}
---
+++ ssgserver/src/main/java/com/isekai/ssgserver/product/entity/ProductDocument.java
@@ -0,0 +1,62 @@
+package com.isekai.ssgserver.product.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Document(indexName = "product")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Mapping(mappingPath = "elastic/product-mapping.json")
+@Setting(settingPath = "elastic/product-setting.json")
+public class ProductDocument {
+
+	@Id
+	private Long id;
+
+	@Field(type = FieldType.Text)
+	private String productName;
+
+	@Field(type = FieldType.Integer)
+	private Integer price;
+
+	@Field(type = FieldType.Text)
+	private String code;
+
+	@Field(type = FieldType.Text)
+	private String productDetail;
+
+	@Field(type = FieldType.Integer)
+	private Integer adultSales;
+
+	@Field(type = FieldType.Integer)
+	private Integer status;
+
+	@Field(type = FieldType.Date)
+	private LocalDateTime createdAt;
+
+	@Builder
+	public ProductDocument(Long id, String productName, Integer price, String code, String productDetail,
+		Integer adultSales, Integer status, LocalDateTime createdAt) {
+
+		this.id = id;
+		this.productName = productName;
+		this.price = price;
+		this.code = code;
+		this.productDetail = productDetail;
+		this.adultSales = adultSales;
+		this.status = status;
+		this.createdAt = createdAt;
+	}
+
+}
